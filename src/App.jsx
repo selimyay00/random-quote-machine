f6d6cc7a780c103493df3293143102d2ae5f4590
selim yay <selimxyay@gmail.com>
2023-11-18 16:21:47 +0300

    useEffect Hook is used to fetch data - added functionality to button

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import { useEffect } from "react"
 
 function App() { 
   // to keep track of the quote appearing in the UI
@@ -7,11 +8,36 @@ function App() {
   // to store all the quotes
   const [allQuotes, setAllQuotes] = useState([])
 
+  // fetch data from an API
+    // inside the useEffect Hook
+
+  useEffect(() => {
+    fetch('https://type.fit/api/quotes')
+      .then(res => res.json())
+      .then(data => {
+        // now all quotes will be stored in allQuotes state
+        setAllQuotes(data)
+        // initial quote will be the first one in allQuotes
+        setCurrentQuote(data[0])
+      })
+  }, [])
+
+  // create a function which generates a random index
+    // for the parameter
+      // use allQuotes length as a limit 
+  function randomQuote(quote) {
+    return quote[Math.floor(Math.random() * allQuotes.length)]quote
+  }
+  
+  function newQuote() {
+    setCurrentQuote(randomQuote(allQuotes))
+  }
+
   return (
     <main>
       <h3>Quote</h3>
       <i>Author</i>
-      <button>New Quote</button>
+      <button onClick={newQuote}>New Quote</button>
     </main>
   )
 }
