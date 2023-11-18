79e1dc3852946f5afd6c6206a0d09df0393f2812
selim yay <selimxyay@gmail.com>
2023-11-18 15:56:14 +0300

    created the basic structure - added necessary states

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-function App() {
-  
+import { useState } from "react"
+
+function App() { 
+  // to keep track of the quote appearing in the UI
+  const [currentQuote, setCurrentQuote] = useState('')
+
+  // to store all the quotes
+  const [allQuotes, setAllQuotes] = useState([])
 
   return (
     <main>
-      <h1>App component rendered!</h1>
+      <h3>Quote</h3>
+      <i>Author</i>
+      <button>New Quote</button>
     </main>
   )
 }
